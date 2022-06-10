019d48af5082f5603dca505b24eb4688f51467bb
Hollen <yujianni666@icloud.com>
2022-06-10 15:14:34 -0400

    Deleted the array deque, it's just so boring. Implemented the randomized deque with guesses

--- Programming Assignment 2/RandomizedQueue.java
+++ Programming Assignment 2/RandomizedQueue.java
@@ -1,35 +1,92 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    public Node<Item> sentinel;
+    public int size;
+    private static class Node<Pokemon> {
+        private Pokemon item;
+        private Node<Pokemon> next;
+        private Node<Pokemon> before;
+        private Node(Pokemon i, Node<Pokemon> n, Node<Pokemon> b) {
+            this.item = i;
+            this.next = n;
+            this.before = b;
+        }
+    }
 
-    // construct an empty randomized queue
     public RandomizedQueue() {
-
+        sentinel = new Node<>(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.before = sentinel;
+        size = 0;
     }
-    // is the randomized queue empty?
+
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
-    // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return this.size;
     }
-    // add the item
-    public void enqueue(Item item) {
-        ;
-    }
-    // remove and return a random item
-    public Item dequeue() {
-        ;
-    }
-    // return a random item (but do not remove it)
-    public Item sample() {
-        ;
-    }
-    // return an independent iterator over items in random order
-    public Iterator<Item> iterator() {
 
+    //    public void addFirst(Item item) {
+//        if (item == null) {
+//            throw new IllegalArgumentException("Can't add null item");
+//        }else {
+//            this.sentinel.next = new Node<>(item, sentinel.next, sentinel);
+//            this.sentinel.next.next.before = this.sentinel.next;
+//            size += 1;
+//        }
+//    }
+//
+//    public void addLast(Item item) {
+//        if (item == null) {
+//            throw new IllegalArgumentException("Can't add null item");
+//        } else {
+//            this.sentinel.before = new Node<>(item, sentinel, sentinel.before);
+//            this.sentinel.before.before.next = this.sentinel.before;
+//            size += 1;
+//        }
+//    }
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Can't add null item");
+        } else {
+            this.sentinel.next = new Node<>(item, sentinel.next, sentinel);
+            this.sentinel.next.next.before = this.sentinel.next;
+            size += 1;
+        }
     }
-    // unit testing (required)
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node<Item> current = sentinel;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+        public void remove(){
+            throw new UnsupportedOperationException("Not supported");
+        }
+        public Item next() {
+            if (size == 0) {
+                throw new NoSuchElementException("Deque is empty");
+            } else {
+                Item it = current.item;
+                current = current.next;
+                return it;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
 }
